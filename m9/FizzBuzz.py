c5229786b1efa2cdd15d2bb8b37ca4d394e39784
roolrd <ruslanriznyk@gmail.com>
2021-02-28 23:40:17 +0200

    added task.9 upd

--- m9/FizzBuzz.py
+++ m9/FizzBuzz.py
@@ -1,12 +1,15 @@
-def fizzbuzz(n):
-    if n%3==0 and n%5==0:
-        return "FizzBuzz"
-    elif n%5==0:
-        return "Buzz"
-    elif n%3==0:
-        return "Fizz"
-    else:
-        return n
+class FizzBuzz:
+    def __init__(self):
+        pass
+    def fizzbuzz(n):
+        if n%3==0 and n%5==0:
+            return "FizzBuzz"
+        elif n%5==0:
+            return "Buzz"
+        elif n%3==0:
+            return "Fizz"
+        else:
+            return n
     
-for i in range(1,100):
-    print(fizzbuzz(i))
+    for i in range(1,100):
+        print(fizzbuzz(i))
